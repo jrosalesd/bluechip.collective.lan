37bbd925c675f24279a128768a6ca57e3774f338
jrosalesd <null>
2020-01-17 13:50:33 +0000

    NEW

--- accordion2.php
+++
@@ -1,4 +0,0 @@
-<div class="panel-group" id="dataentry">
-    <a href="#fileUpload" data-parent="#dataentry" class="btn btn-success"></a>
-    <a href="#singleRecord" data-parent="#dataentry" class="btn btn-success"></a>
-</div>
